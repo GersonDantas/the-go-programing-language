72b94bbc3e1c7c6dd42f6dc29fa9cccea4330839
GersonDantas <gerson.accari@hotmail.com>
2026-07-03 16:19:08 -0300

    [add] multi env: web fetcher - add concurrent fetcher and output file
    Add `fetch.go` for sequential URL fetching and `fetchAll.go` for
    concurrent URL fetching using goroutines and channels. The `fetchAll.go`
    program writes its output to `outputToFetchAll.txt`.

---
+++ fetchAll.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+
+	file, err := os.OpenFile("outputToFetchAll.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	for range os.Args[1:] {
+		line := <-ch // receive from channel
+		_, err := io.WriteString(file, line+"\n")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	_, err = io.WriteString(file, "----------------------------\n")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send error to channel
+		return
+	}
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
